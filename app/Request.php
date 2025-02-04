6c12e6b8f7ce2680768c009659030efd95265269
asd2d <872122@mail.ru>
2025-02-04 22:29:47 +0300

    add functionality for get url and method

---
+++ app/Request.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+class Request
+{
+    public static function getPath()
+    {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';
+        $position = strpos($path, '?');
+
+        if ($position === false) {
+            return $path;
+        }
+        
+        return substr($path, 0, $position);
+    }
+
+    public static function getMethod()
+    {
+        return strtolower($_SERVER['REQUEST_METHOD']);
+    }
+}
